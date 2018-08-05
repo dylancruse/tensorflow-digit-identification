fa8b38dc95ff7af955166e40827197284d245a0b
Dylan Cruse <dylancruse@live.com>
2018-08-05 00:40:39 +0000

    Remove unnecessary code from define_model.py

--- model/define_model.py
+++ model/define_model.py
@@ -25,17 +25,3 @@ Define the model.
 """
 
 smax_model = tf.nn.softmax(tf.matmul(image_vectors, weights) + biases)
-
-"""
-Define training functions.
-Create placeholder for correct answers (one-hot vector with digit labels).
-Implement cross entropy to determine loss of the model.
-Uses backpropagation and gradient descent to tweak Variables to reduce loss. 
-"""
-
-correct_labels = tf.placeholder(tf.float32, [None, 10])
-
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(
-    correct_labels * tf.log(smax_model), reduction_indices=[1]))
-
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
